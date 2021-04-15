b64585c278d6b6aa878c2852a70772a183c125d4
Antoshyn4 <dmytro.antoshyn@nure.ua>
2021-04-15 15:50:21 +0300

    Создал класс узла

---
+++ BinarySearchTree/BinarySearchTree/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+	public class Node<T> : IComparable
+		where T : IComparable
+	{
+		public T Data { set; get; }
+		public Node<T> Left { get; private set; }
+		public Node<T> Right { get; private set; }
+		public Node(T data)
+		{
+			Data = data;
+		}
+		public Node(T data, Node<T> left, Node<T> right)
+		{
+			Data = data;
+			Left = left;
+			Right = right;
+		}
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+				return Data.CompareTo(item.Data);
+            }
+            else
+            {
+				throw new ArgumentException("Не совпаднеие типов");
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Node<T> where T : IComparable
+{
+	public T Data { set; get; }
+	public Node<T> Left { get; private set; }
+	public Node<T> Right { get; private set; }
+	public Node<T>()
+	{
+		sos
+	}
+}
